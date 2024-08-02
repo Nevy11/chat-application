fa28ebed9c2ed9a7731d0091ec49fc3b06451960
nevy11 <smongare2003@gmail.com>
2024-08-02 16:07:41 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nevy11-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'chat-app';
+}
